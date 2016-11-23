fc60884cc19c6e431f9f0e348db04fb28a770f15
Rifki Yandhi <rifkiyandhi@gmail.com>
2016-11-23 12:16:17 +0800

    some values get from input

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,15 +5,15 @@ use Intervention\Image\ImageManager;
 use League\Csv\Reader;
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Application Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register all of the routes for an application.
+  | It is a breeze. Simply tell Lumen the URIs it should respond to
+  | and give it the Closure to call when that URI is requested.
+  |
+ */
 
 $app->get('/', function () use ($app) {
     return view('home');
@@ -24,31 +24,36 @@ $app->get('coupons/generate', function () use ($app) {
 });
 
 $app->post('coupons/generate', function (Request $request) use ($app) {
-    $title = 'momo@'.time();
-    $csvFile = $request->file('coupon_codes');
+    $title        = 'momo@' . time();
+    $csvFile      = $request->file('coupon_codes');
     $templateFile = $request->file('coupon_template');
 
-    $csv = Reader::createFromPath($csvFile->getRealPath());
+    $csv     = Reader::createFromPath($csvFile->getRealPath());
     $csv->setNewline("\r");
     $headers = $csv->fetchOne();
-    $rows = $csv->setOffset(1)->fetchAll();
+    $rows    = $csv->setOffset(1)->fetchAll();
 
     $imageManager = new ImageManager();
 
 
     $filesystem = new \Illuminate\Filesystem\Filesystem();
-    $filesystem->makeDirectory(storage_path('app/coupons/'. $title), 0777, true);
+    $filesystem->makeDirectory(storage_path('app/coupons/' . $title), 0777, true);
 
+    $coordX   = $request->get('text_coord_x');
+    $coordY   = $request->get('text_coord_y');
+    $fontSize = $request->get('font_size');
+    $fontColor = $request->get('font_color');
     //dd($rows);
     foreach ($rows as $row) {
-        $code = $row[0];
+        $code     = $row[0];
         $template = $imageManager->make($templateFile->getRealPath());
-        $template->text($code, 620, 310, function($font){
-            $font->file(storage_path('app/fonts/Lato-Regular.ttf'));
-            $font->size(24);
-            $font->align('center');
+        $template->text($code, $coordX, $coordY, function($font) use($fontSize, $fontColor) {
+            $font->file(storage_path('app/fonts/Lato-Bold.ttf'));
+            $font->size($fontSize);
+            $font->color($fontColor);
+//            $font->align('center');
         });
-        $template->save(storage_path('app/coupons/'. $title.'/'.$code.'.'.$templateFile->getClientOriginalExtension()));
+        $template->save(storage_path('app/coupons/' . $title . '/' . $code . '.' . $templateFile->getClientOriginalExtension()));
     }
 
     echo 'Done';
